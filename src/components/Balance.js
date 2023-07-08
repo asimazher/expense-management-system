6568d3f6d44dc58a2b9da295937c945fcfa97a30
asimazher <asimazher7oct@gmail.com>
2023-07-08 18:58:53 +0500

    transaction-balance-income/expense - implemented

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
 
 const Balance = () => {
+    const { transactions } = useContext(GlobalContext)
+
+    const amounts = transactions.map(transaction => transaction.amount)
+    const total = amounts.reduce((total, item) => (total = total + item), 0).toFixed(2)
+
   return (
     <div>
       <h4>Your Balance</h4>
-      <h1 id='balance'>$ 0.00</h1>
+      <h1>$ {total}</h1>
     </div>
   )
 }
