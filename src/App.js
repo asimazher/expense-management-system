d9474ef816d54e0f0ee9d3a4b3db08cfaeb0a82d
asimazher <asimazher7oct@gmail.com>
2023-07-08 17:08:37 +0500

    project layout created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import AddTransaction from './components/AddTransaction';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
 
 function App() {
   return (
     <div className="">
-     hello world
+      <Header />
+      <div className='container'>
+        <Balance/>
+        <IncomeExpenses />
+        <AddTransaction />
+      </div> 
     </div>
   );
 }
