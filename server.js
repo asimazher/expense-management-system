deab03c6d963fd53c3ba1b75a184117d935fbcb9
asimazher <asimazher7oct@gmail.com>
2023-07-15 17:29:33 +0500

    feature add/delete/get transaction updated to work with MongoDB

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const colors = require('colors')
-const morgan = require('morgan')
-const transactions = require('./routes/transactions')
-const connectDB = require('./config/db')
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const transactions = require("./routes/transactions");
+const connectDB = require("./config/db");
 
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: "./config/config.env" });
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use('/api/v1/transactions', transactions)
+app.use(express.json());
 
-const PORT = process.env.PORT || 5000
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.use("/api/v1/transactions", transactions);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -10,7 +10,7 @@ const Transaction = ({ transaction }) => {
     <div>
         <li className={ transaction.amount < 0 ? 'minus' : 'plus'}>
                 {transaction.text} <span>{sign}$ {Math.abs(transaction.amount)}</span>
-                <button className='delete-btn' onClick={() => deleteTransaction(transaction.id)}>x</button>
+                <button className='delete-btn' onClick={() => deleteTransaction(transaction._id)}>x</button>
         </li>
       
     </div>
